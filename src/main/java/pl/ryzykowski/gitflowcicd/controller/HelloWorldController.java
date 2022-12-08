7ff19c34ceef2ab6151013868e14d763da679cd1
ariello81 <arielr@o2.pl>
2022-12-08 10:15:32 +0100

    przygotowano do scalenia

--- src/main/java/pl/ryzykowski/gitflowcicd/controller/HelloWorldController.java
+++ src/main/java/pl/ryzykowski/gitflowcicd/controller/HelloWorldController.java
@@ -1,16 +1,13 @@
 package pl.ryzykowski.gitflowcicd.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class HelloWorldController {
 
-    @GetMapping("/hello/{name}")
-    public String hello(@PathVariable("name") String name){
+    @GetMapping("/hello")
+    public String hello(@RequestParam("name") String name){
         return "Hello " + name;
     }
 
