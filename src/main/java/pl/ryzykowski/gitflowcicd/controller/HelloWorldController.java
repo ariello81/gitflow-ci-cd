6fcecb1dc184f1767da87f5b20023858f1b5e31e
ariello81 <arielr@o2.pl>
2022-12-08 09:16:54 +0100

    dodano HelloWorldController

---
+++ src/main/java/pl/ryzykowski/gitflowcicd/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package pl.ryzykowski.gitflowcicd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloWorldController {
+
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable("name") String name){
+        return "Hello " + name;
+    }
+
+}
