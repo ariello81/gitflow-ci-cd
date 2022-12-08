061e733b921c41129b59fe97a392afb73ce1e825
ariello81 <arielr@o2.pl>
2022-12-08 08:55:42 +0100

    Initial commit

---
+++ src/main/java/pl/ryzykowski/gitflowcicd/GitflowCiCdApplication.java
@@ -0,0 +1,13 @@
+package pl.ryzykowski.gitflowcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitflowCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitflowCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/ryzykowski/gitflowcicd/GitflowCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ryzykowski.gitflowcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitflowCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
